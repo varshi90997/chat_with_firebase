ebb706ab326b0d619b9dbeadbb0f1afb3683690e
divyabarvaliya <54199646+divyabarvaliya@users.noreply.github.com>
2023-02-25 18:20:05 +0530

    initial commit

---
+++ lib/shared/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:603970858510:web:5f3c75fbd267f0089c965f";
+  static String apiKey = "AIzaSyBGinDulCr8HLTnAqukihEnFDx6Myx5vRY";
+  static String messagingSenderId = "603970858510";
+  static String projectId = "chatapp-559ca";
+  final primaryColor = const Color(0xFFee7b64);
+}
